5075a737eef2c7241cb5060b0e79d11dd69fb4b4
RMMcMurray <156874520+RMMcMurray@users.noreply.github.com>
2025-03-01 03:26:57 -0700

    additional detail

--- Models/player1.js
+++ Models/player1.js
@@ -27,7 +27,23 @@ myGame.player1 = (function () {
             -0.5, -0.5, -0.5, // Bottom base of the model
             0.5, -0.5, -0.5,
             0.5, -0.5, 0.5,
-            -0.5, -0.5, 0.5
+            -0.5, -0.5, 0.5,
+
+            // Additional vertices for more detail
+            0, 0.75, 0, // Mid point of the hat
+            -0.25, 0.5, -0.25, // Mid base of the hat
+            0.25, 0.5, -0.25,
+            0.25, 0.5, 0.25,
+            -0.25, 0.5, 0.25,
+
+            -0.15, 0.5, -0.15, // Mid top base of the body
+            0.15, 0.5, -0.15,
+            0.15, 0.5, 0.15,
+            -0.15, 0.5, 0.15,
+            -0.15, -0.5, -0.15, // Mid bottom base of the body
+            0.15, -0.5, -0.15,
+            0.15, -0.5, 0.15,
+            -0.15, -0.5, 0.15
         ],
 
     // Indices for the wizard model
@@ -38,6 +54,12 @@ myGame.player1 = (function () {
             0, 3, 4,
             0, 4, 1,
 
+            // Additional detail for the hat
+            17, 18, 19,
+            17, 19, 20,
+            17, 20, 21,
+            17, 21, 18,
+
             // Body (cylinder)
             5, 6, 10,
             5, 10, 9,
@@ -47,6 +69,16 @@ myGame.player1 = (function () {
             7, 12, 11,
             8, 5, 9,
             8, 9, 12,
+
+            // Additional detail for the body
+            22, 23, 27,
+            22, 27, 26,
+            23, 24, 28,
+            23, 28, 27,
+            24, 25, 29,
+            24, 29, 28,
+            25, 22, 26,
+            25, 26, 29,
 
             // Base (rectangle)
             13, 14, 15,
@@ -76,7 +108,22 @@ myGame.player1 = (function () {
             ...base,
             ...base,
             ...base,
-            ...base
+            ...base,
+
+            // Additional colors for more detail
+            ...hat,
+            ...hat,
+            ...hat,
+            ...hat,
+
+            ...body,
+            ...body,
+            ...body,
+            ...body,
+            ...body,
+            ...body,
+            ...body,
+            ...body
         ],
     };
     
