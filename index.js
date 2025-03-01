b765449e60f0e3420d4abe243a6b5ce94a1cdf7c
RMMcMurray <156874520+RMMcMurray@users.noreply.github.com>
2025-03-01 00:01:53 -0700

    movement and partle rotation

--- index.js
+++ index.js
@@ -2,31 +2,62 @@ let lastTimeStamp = performance.now();
 
 let inputBuffer = {};
 
-function handleInputs(elapsedTime) {
-    for (let input in inputBuffer) {
-        if (input === 'w') {
-            // Move Left
-        }
+// Get the coordinates of the vertex
+function translateTriangle(dx, dy, dz) {
+    let cost = Math.cos(angle);
+    let sint = Math.sin(angle);
+
+    for (let i = 0; i < (myGame.triangle.vertices.length / 3); i++) {
+        x = myGame.triangle.vertices[(i * 3)]
+        y = myGame.triangle.vertices[(i * 3) + 1]
+        z = myGame.triangle.vertices[(i * 3) + 2]
+
+        x += dx;
+        y += dy;
+        z += dz;
+
+        myGame.triangle.vertices[(i * 3)] = x * cost - y * sint;
+        myGame.triangle.vertices[(i * 3) + 1] = x * sint + y * cost;
     }
 }
 
+
 // Translate the triangle
-function translateTriangle() {
-    // Get the coordinates of the vertex
-    for (let i = 0; i < (myGame.triangle.vertices.length / 3); i++) {
-        let x = myGame.triangle.vertices[(i * 3)];
-        let y = myGame.triangle.vertices[(i * 3) + 1];
-        let z = myGame.triangle.vertices[(i * 3) + 2];
+function handleInputs(elapsedTime) {
+    // Controls Movement Speed
+    let speed = 0.01;
+
+    // Conditional to detect key presses
+    for (let input in inputBuffer) {
+        if (input === 'w') {
+            // Move Forward
+            translateTriangle(0, 0, speed);
+        }
+        else if (input === 'a') {
+            // Move Left
+            translateTriangle(-speed, 0, 0);
+        }
+        else if (input === 's') {
+            // Move Backward
+            translateTriangle(0, 0, -speed);
+        }
+        else if (input === 'd') {
+            // Move Right
+            translateTriangle(speed, 0, 0);
+        }
+        else if (input === ' ') {
+            // Move Up
+            translateTriangle(0, speed, 0);
+        }
+        else if (input === 'Shift') {
+            // Move Down
+            translateTriangle(0, -speed, 0);
+        } 
     }
 }
 
 function update(elapsedTime) {
-    // Rotate the triangle
-    myGame.triangle.rotation[1] += 0.001;
-
-    // Translate the triangle
-    translateTriangle();
-    
+    handleInputs(elapsedTime);
 }
 
 function render(elapsedTime) {
