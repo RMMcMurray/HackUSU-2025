c58958bbe31e8cf7cfec46e5b12b33fcaef79db2
Matthew Larsen <matt4.0larsen@gmail.com>
2025-03-01 03:47:13 -0700

    final refactors

--- index.js
+++ index.js
@@ -186,6 +186,8 @@ function initialize() {
     myGame.render.core.resizeCanvas();
     window.addEventListener('resize', myGame.render.core.resizeCanvas);
 
+    myGame.render.core.initializeModel(model);
+
     requestAnimationFrame(gameLoop);
 }
 
