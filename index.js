ff133952c4fa2222aed6df5e3593a3d5de4f102d
Matthew Larsen <matt4.0larsen@gmail.com>
2025-02-28 23:08:40 -0700

    translation setup

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ function handleInputs(elapsedTime) {
 
 // Translate the triangle
 function translateTriangle() {
-    
+    for (let i = 0; i < (myGame.triangle.vertices.length / 3); i++) {
+        let x = myGame.triangle.vertices[(i * 3)];
+        let y = myGame.triangle.vertices[(i * 3) + 1];
+        let z = myGame.triangle.vertices[(i * 3) + 2];
+    }
 }
+
 function update(elapsedTime) {
     // Rotate the triangle
     myGame.triangle.rotation[1] += 0.001;
